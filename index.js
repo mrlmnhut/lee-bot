137b0870df6bdf4ab833ec8f3391ed744bcd691b
Anh Tuan <hi@duonganhtuan.com>
2021-10-05 18:37:46 +0700

    Add new Dockerfile

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const { EchoBot } = require('./bot');
 
 // Create HTTP server
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
+server.listen(process.env.port || process.env.PORT || 8080, () => {
     console.log(`\n${ server.name } listening to ${ server.url }`);
     console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
